5d83a9806410131d44eb70fa6701294006a6ad89
andreza <naarafreire06@gmail.com>
2023-07-20 15:53:11 -0300

    apresentando dados do banco de dados no template

--- core/views.py
+++ core/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from .models import Produto
 
 def index(request):
+    produtos = Produto.objects.all()
     context = {
         'curso': 'Programação Web com Django Framework',
-        'outro': 'Django é massa'
+        'outro': 'Django é massa',
+        'produtos': produtos
+
     }
     return render(request, 'index.html', context)
 
