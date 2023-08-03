67d3d5298e842e2f4d82c2c9f236a9e2b2d86a6a
andreza <naarafreire06@gmail.com>
2023-08-03 16:05:46 -0300

    configurando rotas e iniciando formulários

--- django2/core/views.py
+++ django2/core/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 
+from .forms import contatoForm
 def index(request):
     return render(request, 'index.html')
 
 def contato(request):
-    return render(request, 'contato.html')
+    form = contatoForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'contato.html', context)
 
 def produto(request):
     return render(request, 'produto.html')
